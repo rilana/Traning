b8713c047488836bd0fb5f26ac9a2ff28f67b4ee
Natasha K <lia_nata_88@mail.ru>
2015-05-17 12:35:30 +0300

    add constructors

--- CheckPoint 1/NewYearsGift/NewYearsGift/Confection.cs
+++ CheckPoint 1/NewYearsGift/NewYearsGift/Confection.cs
@@ -7,12 +7,62 @@ namespace NewYearsGift
 {
     public abstract class Confection:IConfection
     {
+        private int sugar;
+        public int energyvalue;
+        public int gramm;
+
         public string Name { get; set; }
         public Manufacturer Manufacturer { get; set; }
-        public int Sugar{ get;set;}
         public NutritionalValue NutritionalValue { get; set; }
-        public int EnergyValue{ get;set;}
-        public int Gramm{ get;set;}
+        public int Sugar
+        {
+            get 
+            {
+                return sugar; 
+            }
+            set
+            {
+                if (sugar >= 0) sugar = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public int EnergyValue
+        {
+            get
+            {
+                return energyvalue;
+            }
+            set
+            {
+                if (energyvalue >= 0) energyvalue = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public int Gramm
+        {
+            get
+            {
+                return gramm;
+            }
+            set
+            {
+                if (gramm >= 0) gramm = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public Confection()
+        { }
+        public Confection(string name, Manufacturer Manufacturer, NutritionalValue NutritionalValue,int Sugar,
+            int EnergyValue,int Gramm)
+        {
+            this.Name = Name;
+            this.Manufacturer = Manufacturer;
+            this.NutritionalValue = NutritionalValue;
+            this.Sugar = Sugar;
+            this.EnergyValue = EnergyValue;
+            this.Gramm = Gramm;
+
+        }
 
        
         
